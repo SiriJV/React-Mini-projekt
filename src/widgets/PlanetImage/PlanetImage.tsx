04cf9e462e620429ce2b61662f5cd491e6d9df84
SiriJV <siri.viren@icloud.com>
2025-02-21 09:58:22 +0100

    Added create image

---
+++ src/widgets/PlanetImage/PlanetImage.tsx
@@ -0,0 +1,12 @@
+import './PlanetImage.scss';
+
+const PlanetImage = () => {
+    return (
+    <section className="PlanetImage">
+        <h2>Planet image</h2>
+        <img src="https://science.nasa.gov/wp-content/uploads/2024/03/pia15162-mercury-basins-messenger-16x9-1.jpg?resize=1536,864"></img>
+    </section>
+    )
+}
+
+export default PlanetImage;
