90c63c293b6536ceb3e0b8e851720c698f5fe868
SiriJV <siri.viren@icloud.com>
2025-02-21 09:59:23 +0100

    Styled app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import './App.scss'
 import SpaceAccordion from './widgets/SpaceAccordion/SpaceAccordion'
+import AgeCalculator from './widgets/AgeCalculator/AgeCalculator'
+import PlanetImage from './widgets/PlanetImage/PlanetImage';
+import AlienNameGenerator from './widgets/AlienNameGenerator/AlienNameGenerator';
+import CreatePlanets from './widgets/CreatePlanets/CreatePlanets';
 
 function App() {
   return (
     <>
-      <h1>React </h1>
+      <h1>Space Hub (React Widget Practice)</h1>
+      <main>
       <SpaceAccordion />
+      <AgeCalculator />
+      <AlienNameGenerator />
+      <PlanetImage />
+      <CreatePlanets />
+      </main>
     </>
   )
 }
